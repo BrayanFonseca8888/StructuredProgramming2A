39a9f7cd7e92540a939225562a00c8b763ace535
BrayanFonseca8888 <84408693+BrayanFonseca8888@users.noreply.github.com>
2021-05-23 23:05:37 -0500

    Create activity_02.py

---
+++ activity_02.py
@@ -0,0 +1,9 @@
+bank_account=1500
+product = int(input("What is the cost of the product?"))
+
+if(product <= bank_account):
+    print("Payment succesful")
+else:
+    print("Payment rejected")
+
+print("End program")
