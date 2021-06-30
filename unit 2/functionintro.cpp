ed48e5a6af0341de2c35f88b723ae770190e0130
BrayanFonseca8888 <84408693+BrayanFonseca8888@users.noreply.github.com>
2021-06-30 00:13:07 -0500

    Create functionintro.cpp

---
+++ unit 2/functionintro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <tuple>
+
+std::string myString = "Hola mundo";
+int myNumber = 3;
+float myFloat  = 3.1416;
+
+std::tuple<int, std::string > myTuple = {  1, "string"  };
+
+
+void sayHello(  std::string  tag1    );
+
+
+
+int main(){
+
+        // myTuple = {}
+
+        sayHello(  myString  ); 
+
+        return EXIT_SUCCESS;
+}
+
+void sayHello(  std::string  tag1    ){
+        std::cout<<tag1<<std::endl;
+}
