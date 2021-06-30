619bd79f7880cea4d2829832fa88c590da56af9d
BrayanFonseca8888 <84408693+BrayanFonseca8888@users.noreply.github.com>
2021-06-30 00:25:36 -0500

    Create functionintro.c

---
+++ unit 2/functionintro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//#include "./utils.h"
+#include "/mnt/c/Users/HP/StructuredPrograming2A/unit_2/folderTest/utils.h"
+
+int myNumber = 3;
+float myFloat  = 3.1416;
+char myString[344] = "Hola mundo";
+
+int main(  ){
+        sayHello(  "Hello world" ); 
+        sayMyName( "brayan" );
+        int myResult =     addTwoNumbers(  3, 4   );
+        printf( "myResult is equal to:  %d \n",   myResult  );
+
+        printf("%d", libInteger  );
+        return 0;
+}
