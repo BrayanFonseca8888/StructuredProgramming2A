bf7a7a83a9e609e0075e1b4f3a80c22519d615bb
BrayanFonseca8888 <84408693+BrayanFonseca8888@users.noreply.github.com>
2021-08-04 13:33:23 -0500

    Create practica01.py

---
+++ unit 2/practica01.py
@@ -0,0 +1,11 @@
+import sys
+
+###Practica###
+
+def sumLista (a):
+    return[sum([input[i] for i in range (0, len(a)) if i%2==0]), sum([input[i] for i in range (0, len(a)) if i%2==1])]
+
+if __name__ == "__main__":
+    input=[50, 60, 60, 45, 70]
+
+    print(sumLista(input))
