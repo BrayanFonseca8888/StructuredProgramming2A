913e79686308b1e22a0fa5471c215a3c488160b4
BrayanFonseca8888 <84408693+BrayanFonseca8888@users.noreply.github.com>
2021-08-04 13:15:12 -0500

    Create callback.c

---
+++ unit 2/callback.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include "stdbool.h"
+
+#include "swap.h"
+
+
+
+void callTonumber(int number){
+    //Logic
+    printf("Calling %d...\n", number);
+}
+
+void sendMessageNumber(int number){
+    printf("sending message to %d \n...",number);
+}
+
+void Securitycamera(void (*cb)(int number), int EmergencyNumber);
+
+int main(){
+    // for(;;){
+        Securitycamera(callTonumber,911);
+        Securitycamera(sendMessageNumber,911);
+    
+
+
+    return 0;
+}
+
+void Securitycamera(void (*cb)(int number), int EmergencyNumber){
+    //Lógica compleja// 
+    bool thief = false;
+    if (thief){
+        cb(EmergencyNumber);
+    } //Si detecta ladrón, debe hacer algo
+}
